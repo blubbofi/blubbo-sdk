4379ff3fa1e1fac93b2785d95f4e01e3ca404af0
9oelM <hj923@hotmail.com>
2024-10-24 08:49:08 +0900

    fix: uint assertions

--- src/math.ts
+++ src/math.ts
@@ -12,12 +12,12 @@ export class ScMath {
   public static MAX_UINT_257 = 2n ** 257n - 1n;
 
   static uint_sub(a: bigint, b: bigint): bigint {
-    assert(a > 0, "a must be greater than 0");
-    assert(b > 0, "b must be greater than 0");
+    assert(a >= 0, "a must be gte 0");
+    assert(b >= 0, "b must be gte 0");
 
     const result = a - b;
 
-    assert(result > 0, "result must be greater than 0");
+    assert(result >= 0, "result must be gte 0");
 
     assert(
       result <= this.MAX_UINT_257,
@@ -28,8 +28,8 @@ export class ScMath {
   }
 
   static uint_add(a: bigint, b: bigint): bigint {
-    assert(a > 0, "a must be greater than 0");
-    assert(b > 0, "b must be greater than 0");
+    assert(a >= 0, "a must be gte 0");
+    assert(b >= 0, "b must be gte 0");
 
     const result = a + b;
 
@@ -43,8 +43,8 @@ export class ScMath {
 
   // This function assumes `b` is scaled by `SCALE`
   static uint_mul(a: bigint, b: bigint): bigint {
-    assert(a > 0, "a must be greater than 0");
-    assert(b > 0, "b must be greater than 0");
+    assert(a >= 0, "a must be gte 0");
+    assert(b >= 0, "b must be gte 0");
 
     const scaled_product = a * b;
 
@@ -60,7 +60,7 @@ export class ScMath {
 
   // This function assumes `b` is scaled by `SCALE`
   static uint_div(a: bigint, b: bigint): bigint {
-    assert(a > 0, "a must be greater than 0");
+    assert(a >= 0, "a must be gte 0");
     assert(b > 0, "b must be greater than 0");
 
     const scaled_product = a * this.SCALE;
@@ -76,7 +76,7 @@ export class ScMath {
   }
 
   static uint_scale_pct(pct: bigint) {
-    assert(pct > 0, "pct must be greater than 0");
+    assert(pct >= 0, "pct must be gte 0");
     assert(pct <= 100, "pct must be less than or equal to 100");
 
     return (pct * this.SCALE) / 100n;
@@ -84,9 +84,9 @@ export class ScMath {
 
   // This function assumes `b` is scaled by `10 ^ b_decimals`
   static uint_mul_decimals(a: bigint, b: bigint, b_decimals: bigint): bigint {
-    assert(a > 0, "a must be greater than 0");
-    assert(b > 0, "b must be greater than 0");
-    assert(b_decimals > 0, "b_decimals must be greater than 0");
+    assert(a >= 0, "a must be gte 0");
+    assert(b >= 0, "b must be gte 0");
+    assert(b_decimals >= 0, "b_decimals must be gte 0");
 
     const scale = 10n ** b_decimals;
     const scaled_product = a * b;
@@ -103,9 +103,9 @@ export class ScMath {
 
   // This function assumes `b` is scaled by `10 ^ b_decimals`
   static uint_div_decimals(a: bigint, b: bigint, b_decimals: bigint): bigint {
-    assert(a > 0, "a must be greater than 0");
+    assert(a >= 0, "a must be gte 0");
     assert(b > 0, "b must be greater than 0");
-    assert(b_decimals > 0, "b_decimals must be greater than 0");
+    assert(b_decimals >= 0, "b_decimals must be gte 0");
 
     const scale = 10n ** b_decimals;
     const scaled_product = a * scale;
