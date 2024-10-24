f73ab78f8f4879fcaba72691e04552d441b1752d
9oelM <hj923@hotmail.com>
2024-10-24 22:41:47 +0900

    feat: get jetton wallet from address

--- src/jetton/index.ts
+++ src/jetton/index.ts
@@ -1,3 +1,31 @@
+import { Address, TonClient } from "@ton/ton";
+import { JettonMinter } from "./jetton_minter";
+import { JettonWallet } from "./jetton_wallet";
+
 export * from "./jetton_constants";
 export * from "./jetton_minter";
 export * from "./jetton_wallet";
+
+/**
+ * Get a jetton wallet for an owner address.
+ * Works on standard jetton minter/wallet contracts.
+ * Does not work on nonstandard jetton minter contracts that DO NOT
+ * implement `get_wallet_address`.
+ */
+export async function getStandardJettonWalletForAddress(args: {
+  tonClient: TonClient;
+  address: {
+    owner: Address;
+    jettonMinter: Address;
+  };
+}) {
+  const minter = args.tonClient.open(
+    JettonMinter.createFromAddress(args.address.jettonMinter),
+  );
+  const jettonWalletAddress = await minter.getWalletAddress(args.address.owner);
+  const jettonWallet = args.tonClient.open(
+    JettonWallet.createFromAddress(jettonWalletAddress),
+  );
+
+  return jettonWallet;
+}
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,5 +1,9 @@
-import { ConstantsByDeployment, ContractInteraction } from "../src/index";
-import { TonClient } from "@ton/ton";
+import {
+  ConstantsByDeployment,
+  ContractInteraction,
+  getStandardJettonWalletForAddress,
+} from "../src/index";
+import { Address, TonClient } from "@ton/ton";
 
 describe("Smoke tests", () => {
   it(`should get reserve`, async () => {
@@ -26,5 +30,34 @@ describe("Smoke tests", () => {
     } catch (e) {
       fail(e);
     }
+  });
+
+  it(`should get jetton wallet for owner address`, async () => {
+    const client = new TonClient({
+      // We're on mainnet for this one
+      endpoint: "https://toncenter.com/api/v2/jsonRPC",
+    });
+
+    // https://tonviewer.com/EQAVkAV5mRv5O1H6qZNRuhY4FdJlInqoz4n-_LThUZ0TbJxE/jetton/EQC98_qAmNEptUtPc7W6xdHh_ZHrBUFpw5Ft_IzNU20QAJav
+    const expectedJettonWalletAddress = Address.parse(
+      `EQC0yj5mT3jND5VWPCpAC_nqErRMtXyurNO291J4PcWjmi1I`,
+    );
+
+    const jettonWalletAddress = await getStandardJettonWalletForAddress({
+      tonClient: client,
+      address: {
+        owner: Address.parse(
+          `UQAVkAV5mRv5O1H6qZNRuhY4FdJlInqoz4n-_LThUZ0TbMGB`,
+        ),
+        // Mainnet tsTON jetton minter
+        jettonMinter: Address.parse(
+          `EQC98_qAmNEptUtPc7W6xdHh_ZHrBUFpw5Ft_IzNU20QAJav`,
+        ),
+      },
+    });
+
+    expect(
+      jettonWalletAddress.address.equals(expectedJettonWalletAddress),
+    ).toBe(true);
   });
 });
